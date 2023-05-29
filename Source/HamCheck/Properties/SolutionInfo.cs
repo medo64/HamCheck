27c6f17c6bf1667edc83ac0c45286ba0090c4168
Josip Medved <jmedved@jmedved.com>
2023-05-29 10:24:23 -0700

    Bumped version

--- Source/HamCheck/Properties/SolutionInfo.cs
+++ Source/HamCheck/Properties/SolutionInfo.cs
@@ -5,5 +5,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("Josip Medved")]
 [assembly: AssemblyCopyright("Copyright (c) 2015 Josip Medved <jmedved@jmedved.com>")]
 
-[assembly: AssemblyVersion("1.40.*")]
-[assembly: AssemblyInformationalVersion("1.40")]
+[assembly: AssemblyVersion("1.50.*")]
+[assembly: AssemblyInformationalVersion("1.50")]
